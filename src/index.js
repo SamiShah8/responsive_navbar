dbef6ba41cbcf5a3e3ff066f96fcd1c31f8c5f0b
sami <shahsamiksha54@gmail.com>
2025-01-05 06:12:42 +0000

    completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("hello")
+const crossBtn = document.getElementById("x-button");
+const showBtn = document.getElementById("hamburger-button");
+const mobileNav = document.getElementById("mb_nav");
+
+const crossClickHandler = function () {
+  mobileNav.style.transform = "translateX(100%)";
+};
+
+const showClickHandler = function () {
+  mobileNav.style.transform = "translateX(0)";
+};
+
+showBtn.addEventListener("click", showClickHandler);
+crossBtn.addEventListener("click", crossClickHandler);
